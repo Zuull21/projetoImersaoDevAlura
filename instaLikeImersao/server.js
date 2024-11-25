fe4a7de75148e4a9480b332ace97739f23bbdf6b
Zull21 <julio.dahi@sptech.school>
2024-11-25 01:28:43 -0300

    feat/projeto-finalizado

--- instaLikeImersao/server.js
+++ instaLikeImersao/server.js
@@ -1,52 +1,11 @@
-import express from "express"
-
-const posts = [
-    {
-        id: "1",
-        descricao: "Teste1",
-        imagem: "https://placecats.com/millie/300/150"
-    },
-    {
-        id: "2",
-        descricao: "Teste2",
-        imagem: "https://placecats.com/millie/300/150"
-    },
-    {
-        id: "3",
-        descricao: "Teste3",
-        imagem: "https://placecats.com/millie/300/150"
-    },
-    {
-        id: "4",
-        descricao: "Teste4",
-        imagem: "https://placecats.com/millie/300/150"
-    },
-    {
-        id: "5",
-        descricao: "Teste5",
-        imagem: "https://placecats.com/millie/300/150"
-    },
-];
+import express from "express";
+import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
-app.use(express.json());
+
+app.use(express.static("uploads"));
+routes(app);
 
 app.listen(3000, () => {
-    console.log("Executando...");
+    console.log("Servidor escutando ...");
 });
-
-app.get("/posts", (req, res) => {
-    res.status(200).json(posts); 
-});
-
-function postPorId(id){
-    return posts.findIndex((post) => {
-        return post.id === Number(id)
-    });
-};
-
-app.get("/posts/:id", (req, res) => {
-    const index = postPorId(req.params.id)
-    res.status(200).json(posts[index]); 
-});
-
